64cc538c8d66bba3194a95d6b5ad77964d8d107c
MuddyBootsCode <firsttogo98@gmail.com>
2017-11-05 16:30:48 -0600

    added error handling for book cover not existing

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,15 +6,17 @@ class Book extends Component {
     static propTypes = {
         book: PropTypes.object.isRequired
     }
+
     render() {
 
         const { book } = this.props
+        const bookCover = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : "No Cover"
 
         return (
             <li key={book.id}>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookCover})`}}></div>
                         <div className="book-shelf-changer">
                             <select>
                                 <option value="none" disabled>Move to...</option>
